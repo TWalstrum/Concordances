4f63db54ed72e0e73757d94dcb790fc8df510734
@TWalstrum <thom.walstrum@gmail.com>
2023-10-14 06:03:30 -0500

    Update paths.

--- census_occ/code/census_occ_10_18.R
+++ census_occ/code/census_occ_10_18.R
@@ -18,7 +18,7 @@ library(readxl)
 # The Excel file is a slightly cleaned version of references/table-h1_h2.xlsx,
 # sheet Template_Back_DO NOT EDIT
 census_occ_10_18 <-
-  read_excel(str_glue("data/raw/conversion_rates.xlsx")) |>
+  read_excel("census_occ/data/dirty/conversion_rates.xlsx") |>
   mutate(
     occ10 = as.integer(occ10),
     occ18 = as.integer(occ18)
@@ -26,4 +26,4 @@ census_occ_10_18 <-
   fill(soc10, occ10, soc10_nm) |>
   select(!type) |>
   drop_na() |>
-  write_csv(str_glue("data/clean/occ_10_18.csv"))
+  write_csv(str_glue("census_occ/data/clean/occ_10_18.csv"))
